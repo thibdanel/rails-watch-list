bce13077f9330b2813fd60205a96e738668d75c0
Thibault Danel <thibdanel@hotmail.fr>
2022-02-18 23:33:42 +0100

    modif banner

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   end
   #delete '/bookmarks/:id', to: 'bookmarks#destroy'
   resources :bookmarks, only: :destroy
+  resources :lists, only: :destroy
 end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
